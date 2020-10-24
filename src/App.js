3b81ae2e3462e4be867ad04f33675bf737ddd878
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-10-24 09:56:00 -0500

    added search query

--- src/App.js
+++ src/App.js
@@ -8,22 +8,49 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a
 function App() {
   // set the movies when we get the movies from the api
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   // useEffect hook 
   useEffect(() => {
-    fetch(FEATURED_API)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setMovies(data.results);
-      })
+    getMovies(FEATURED_API);
   }, [])
 
+  const getMovies = (API) => {
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setMovies(data.results);
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API+searchTerm)
+      setSearchTerm("");
+    }
+
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
 
   return (
     <div>
       <header>
-        <input className="search" type="search" placeholder="Search..." />
+        <form onSubmit={handleOnSubmit}>
+          <input 
+            className="search" 
+            type="search" 
+            placeholder="Search..." 
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+
+        </form>
       </header>
       <div className="movie-container">
         {movies.length > 0 && movies.map((movie) => (
